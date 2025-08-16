024ae1a4b33ae476f2c88cf78b52abe2f2805460
zhukovv <you@example.com>
2025-08-16 15:17:59 +0500

    second homework

---
+++ ConsoleApp_0/homeWorks/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("give me a num, sir");
+
+try
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num % 2 != 0) { Console.WriteLine("чётное"); } else { Console.WriteLine("нечётное"); }
+}
+catch
+{
+    Console.WriteLine("you're wrong !!! ");
+}
