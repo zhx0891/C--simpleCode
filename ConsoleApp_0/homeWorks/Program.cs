0d718a15bdcb55b5bfb0c1c852fef809f127792f
zhukovv <you@example.com>
2025-08-20 20:21:20 +0500

    second homework

--- ConsoleApp_0/homeWorks/Program.cs
+++ ConsoleApp_0/homeWorks/Program.cs
@@ -1,12 +1,55 @@
 ﻿
-Console.WriteLine("give me a num, sir");
+//Console.WriteLine("give me a num, sir");
+
+//try
+//{
+//    int num = int.Parse(Console.ReadLine());
+//    if (num % 2 != 0) { Console.WriteLine("чётное"); } else { Console.WriteLine("нечётное"); }
+//}
+//catch
+//{
+//    Console.WriteLine("you're wrong !!! ");
+//}
 
 try
 {
-    int num = int.Parse(Console.ReadLine());
-    if (num % 2 != 0) { Console.WriteLine("чётное"); } else { Console.WriteLine("нечётное"); }
+    Console.WriteLine("первое число? ");
+    double num = double.Parse(Console.ReadLine());
+    Console.WriteLine("второе число? ");
+    double num2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("выбери действие + - / * ");
+    string act = Console.ReadLine();
+
+    //if (act == "+") { Console.WriteLine($"{num} + {num2} = {num + num2}"); }
+    //if (act == "-") { Console.WriteLine($"{num} - {num2} = {num - num2}"); }
+    //if (act == "/") { Console.WriteLine($"{num} / {num2} = {num / num2}"); }
+    //if (act == "*") { Console.WriteLine($"{num} * {num2} = {num * num2}"); }
+    //else
+    //{
+    //    Console.WriteLine("you're wrong with action !!! ");
+    //}
+    switch (act)
+    {
+        case "+" :
+            Console.WriteLine($"{num} + {num2} = {num + num2}");
+            break;
+        case "-" :
+            Console.WriteLine($"{num} - {num2} = {num - num2}");
+            break;
+        case "*":
+            Console.WriteLine($"{num} * {num2} = {num * num2}");
+            break;
+        case "/":
+            Console.WriteLine($"{num} / {num2} = {num / num2}");
+            break;
+            Console.WriteLine("you're wrong with action !!! ");
+        default:
+
+            break;
+    }
 }
 catch
 {
     Console.WriteLine("you're wrong !!! ");
 }
+
